ebc35400c456a56552b87cacd18f5b39c1d70b8c
ChengHan <wow0422796353@gmail.com>
2024-06-05 00:02:56 +0800

    修正 - 點餐bug

--- backendApp/module/mqtt.py
+++ backendApp/module/mqtt.py
@@ -2,12 +2,12 @@ import paho.mqtt.client as mqtt
 
 def send_mqtt_message(message, topic, qos=1):
     client = mqtt.Client()
-    host = "140.131.115.152"
+    host = "broker.MQTTGO.io"
     port = 1883 
-    username = "wow0422796353"
-    password = "sunkaiwj00"
-    if username and password:
-        client.username_pw_set(username, password)
+    # username = "wow0422796353"
+    # password = "sunkaiwj00"
+    # if username and password:
+    #     client.username_pw_set(username, password)
     client.connect(host, port)
     client.publish(topic, message, qos=qos)
     print('成功發送mqtt')
