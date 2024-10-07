aab14960377af4cb8dcd2d87d4d27e91782de6d0
ChengHan <wow0422796353@gmail.com>
2024-10-07 14:47:21 +0800

    新增 - 語音檔案及apikey誤殖問題

--- lineIntegrations/views/medicament.py
+++ lineIntegrations/views/medicament.py
@@ -2,6 +2,7 @@ import json
 import openai
 import edge_tts
 import subprocess
+import base64
 from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import render
 from django.http import JsonResponse
@@ -10,19 +11,19 @@ from backendApp.models import Patient
 from lineIntegrations.module.lineVerify import getLineUserUidByToken
 from pydub import AudioSegment
 
+voice_param = "zh-TW-HsiaoChenNeural"
 
 # @line_verify_2
 # @csrf_exempt
-def sendMessageToOpenAi(request, *args, **kwargs):
+async def sendMessageToOpenAi(request, *args, **kwargs):
     if request.method == 'POST':
-        patient_id = kwargs.get('patient_id')
+        # patient_id = kwargs.get('patient_id')
         data = json.loads(request.body)
 
-        print(data)
-        openai.api_key = api_key
+        openai.api_key = 'test'
 
-        transcript = "用繁體中文回答" + data.get('transcript', '')
-
+        transcript = "用繁體中文回答：" + data.get('transcript', '')
+    
         response = openai.ChatCompletion.create(
             model="gpt-3.5-turbo",
             messages=[{"role": "user", "content": transcript}],
@@ -31,44 +32,35 @@ def sendMessageToOpenAi(request, *args, **kwargs):
         if response and response.choices:
             message = response.choices[0].message
             if message:
-                response_text = message.content
-                response_role = message.role
-            else:
-                response_text = "No response generated."
-                response_role = "Unknown"
+                # response_text = message.content
+                # response_role = message.role
+                response_text = "當然可以，請問你需要什麼幫助呢？我會盡力幫你解決問題。"
+                response_role = "assistant"
+                audio_path = "static/audio/output2.mp3"
+                communicate = edge_tts.Communicate(text=response_text, voice=voice_param)
+                await communicate.save(audio_path)
+                
+                wav_path = audio_path.replace('mp3','wav')
+                lipsync_path = wav_path.replace('wav','json')
+                
+                convert_mp3_to_wav(audio_path, wav_path)
+                convert_wav_to_lipsyncJson(wav_path, lipsync_path)
+        if response_text:
+            audioBase64 = convert_audio_to_base64(audio_path);
+            lipsync_data = read_json_file(lipsync_path)
+            data = {
+                'text': response_text,
+                'role': response_role,
+                "audio":  audioBase64,
+                "lipsync": lipsync_data,
+                "facialExpression": "smile",
+                "animation": "Laughing",
+            }
+            return JsonResponse(data)
         else:
-            response_text = "No response generated."
-            response_role = "Unknown"
-        return JsonResponse({'response': response_text, 'role': response_role})
+            return JsonResponse({'error': '未知錯誤'}, status=405)
     else:
         return JsonResponse({'error': 'Unsupported HTTP method'}, status=405)
-
-async def textToSpeech(request):
-    if request.method == 'POST':
-        text = "哈摟 我是智伴系統，歡迎與我聊天，也歡迎你們與我進行健康諮詢我將提供你們建議，若是身體不適也可以透過旁邊的按鈕通知給照護者喔！"
-        voice = "zh-TW-HsiaoChenNeural"
-        communicate = edge_tts.Communicate(text=text, voice=voice)
-
-        audio_path = "static/audio/output2.mp3"  # 指定音频文件的存储路径
-        await communicate.save(audio_path)
-        
-        wav_path = audio_path.replace('mp3','wav')
-        lipsync_path = wav_path.replace('wav','json')
-
-        convert_mp3_to_wav(audio_path, wav_path)
-        convert_wav_to_lipsyncJson(wav_path, lipsync_path)
-
-        # 之後若是虛擬人跟django運行於不同電腦則須改用外部存取方式
-        response_data = {
-            "audio": audio_path,
-            "lipsync": lipsync_path,
-            # animation、facialExpression兩個得靠情緒分析獲取、目前先寫死
-            "facialExpression": "smile",
-            "animation": "Laughing",
-        }    
-        return JsonResponse(response_data)
-    else:
-        return JsonResponse({'error': 'Invalid request'}, status=400)
 
 @csrf_exempt
 def setSessionByToken(request):
@@ -108,3 +100,13 @@ def convert_wav_to_lipsyncJson(input_wav, output_json):
         subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     except:
         print('請安裝"wav轉嘴型json檔案套件"，並設定環境變數 - rhubarb lipsync (參考 readme)')
+
+def convert_audio_to_base64(filepath):
+    with open(filepath, "rb") as audio_file:
+        audio_data = audio_file.read()
+        audio_base64 = base64.b64encode(audio_data).decode('utf-8')
+    return audio_base64
+
+def read_json_file(file_path):
+    with open(file_path, 'r', encoding='utf-8') as file:
+        return json.load(file)
