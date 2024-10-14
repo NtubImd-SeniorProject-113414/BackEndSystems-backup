2c758a000407ee6eee3960fedf24ed19f176f643
11046083 <11046083@ntub.edu.tw>
2024-10-14 17:47:26 +0800

    修改 - 餐點配送更新

--- backendApp/module/mqtt.py
+++ backendApp/module/mqtt.py
@@ -2,12 +2,12 @@ import paho.mqtt.client as mqtt
 
 def send_mqtt_message(message, topic, qos=1):
     client = mqtt.Client()
-    host = "broker.MQTTGO.io"
+    host = "140.131.115.42"
     port = 1883 
-    # username = "wow0422796353"
-    # password = "sunkaiwj00"
-    # if username and password:
-    #     client.username_pw_set(username, password)
+    username = "medimate"
+    password = "t;65l4u xul6vu4wj/3"
+    if username and password:
+        client.username_pw_set(username, password)
     client.connect(host, port)
     client.publish(topic, message, qos=qos)
     print('成功發送mqtt')
